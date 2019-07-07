66984f5094a9fb8552163fcfd24db3e7751c74fe
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-07-07 15:46:07 -0600

    create react template

---
+++ README.md
@@ -0,0 +1,13 @@
+# Image Gallery Using React
+
+## Getting started
+Git and Clone Repo
+```
+npm install
+```
+```
+npm start
+```
+
+## Screenshots
+
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Image Gallery using React</h2>
+        </div>
+        <p className="App-intro">
+          Create an image gallery by mapping urls and applying Flexbox styling
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
